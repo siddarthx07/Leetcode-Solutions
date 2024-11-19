ad9b7275a6f28963381ec964a49fdd7975963554
Siddarth Bandi <107579432+siddarthx07@users.noreply.github.com>
2024-11-20 03:32:23 +0530

    Time: 0 ms (100%), Space: 7.8 MB (68.78%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int j=0;
+        for (int i=0;i<t.size();i++)
+        {
+            if(s[j]==t[i])
+            {
+                j++;
+
+            }
+            else
+            {
+                continue;
+            }
+
+        }
+        if(j==s.size())
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+};
