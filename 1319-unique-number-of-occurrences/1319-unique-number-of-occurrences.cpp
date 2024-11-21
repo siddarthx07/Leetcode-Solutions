a6878cf8a7247e47c339737cc271fabce912d436
Siddarth Bandi <107579432+siddarthx07@users.noreply.github.com>
2024-11-21 07:32:02 +0530

    Time: 0 ms (100%), Space: 11.3 MB (82.47%) - LeetHub

---
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        map<int,int> mp;
+        for (int i=0;i<arr.size();i++)
+        {
+            mp[arr[i]]++;
+        }
+        map<int,int>mp2;
+        for(auto x: mp)
+        {
+            mp2[x.second]++;
+        }
+        for(auto y: mp2)
+        {
+            if(y.second>1)
+            {
+                return false;
+            }
+        }
+        return true;
+
+    }
+};
