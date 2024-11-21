069579c91ce8ca7351844b1bf6b93b8e71cf4416
Siddarth Bandi <107579432+siddarthx07@users.noreply.github.com>
2024-11-21 12:01:14 +0530

    Time: 0 ms (100%), Space: 114.4 MB (62.35%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+
+        int n1=INT_MAX;
+        int n2=INT_MAX;
+        for (int n: nums)
+        {
+            if(n<=n1)
+            {
+                n1=n;
+            }
+            else if(n<=n2)
+            {
+                n2=n;
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+
+
+    }
+};
