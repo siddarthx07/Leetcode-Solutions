93fb49660b4c7089a257b7561901213aabd235c9
Siddarth Bandi <107579432+siddarthx07@users.noreply.github.com>
2024-12-08 03:35:17 +0530

    Time: 0 ms (100%), Space: 40.2 MB (40.91%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -5,7 +5,7 @@ public:
     vector<int> result(n, 1);
     int prefix = 1;
     for (int i = 0; i < n; i++) {
-        result[i] *= prefix;
+        result[i] = prefix;
         prefix *= nums[i];
     }
         int suffix = 1;
